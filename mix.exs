2651979ac22724283fbbdab3739d9c2377451a51
mirage1129 <meraj.a.qazi@gmail.com>
2018-12-24 15:29:57 +0800

    fixed title issues in show guide

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lani.MixProject do
   def project do
     [
       app: :lani,
-      version: "0.1.9",
+      version: "0.2.0",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
